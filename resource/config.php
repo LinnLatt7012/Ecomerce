7815b82ddb1523aec2bd03489eac55f5f2e3f8bd
plinnlatt <plinnlatt70@gmail.com>
2021-09-27 18:12:24 +0630

    my sql updated

--- resource/config.php
+++ resource/config.php
@@ -11,13 +11,13 @@ defined ("TEMPLATE_BACK") ? null: define("TEMPLATE_BACK", __DIR__.DS."templates\
 defined ("UPLOAD_DIR") ? null: define("UPLOAD_DIR", __DIR__.DS.$image_directory);
 //echo "<h1>".TEMPLATE_FRONT."</h1>";
 
-defined ("DB_HOST") ? null: define("DB_HOST","localhost");
-defined ("DB_USER") ? null: define("DB_USER","root");
+defined ("DB_HOST") ? null: define("DB_HOST","mysql-52092-0.cloudclusters.net");
+defined ("DB_USER") ? null: define("DB_USER","admin");
 
-defined ("DB_PASS") ? null: define("DB_PASS","");
+defined ("DB_PASS") ? null: define("DB_PASS","70128910");
 defined ("DB_NAME") ? null: define("DB_NAME","ecom_db");
 
-$connection =mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+$connection =mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME,"10666");
 
 require_once("function.php");
 
