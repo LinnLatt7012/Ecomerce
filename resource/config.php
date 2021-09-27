2d25abeb18e49162e841542ab440eddcb1d8cea3
plinnlatt <plinnlatt70@gmail.com>
2021-09-27 18:19:42 +0630

    css test

--- resource/config.php
+++ resource/config.php
@@ -18,7 +18,7 @@ defined ("DB_PASS") ? null: define("DB_PASS","70128910");
 defined ("DB_NAME") ? null: define("DB_NAME","ecom_db");
 
 $connection =mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME,"10666");
-
+echo TEMPLATE_FRONT;
 require_once("function.php");
 
 ?>
